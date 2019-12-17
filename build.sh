5c5fe18eccaba4d526a90243afa48f46ee1fbd43
Adeel Kazmi <adeel.kazmi@samsung.com>
2019-12-17 12:44:12 +0000

    Updated build.sh to work behind a proxy
    Change-Id: I5d7386b5a327e5c5c26757998885f07d56715724

--- build.sh
+++ build.sh
@@ -1,61 +1,89 @@
-if [ ! -d "$ANDROID_SDK" ]; then
-if [ ! -d "$HOME/Android/Sdk" ]; then
-mkdir -p "$HOME/Android/Sdk"
-cd "$HOME/Android/Sdk"
-wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-unzip sdk-tools-linux-4333796.zip
-tools/bin/sdkmanager --update
-yes | tools/bin/sdkmanager "patcher;v4" "platform-tools" "platforms;android-29" "build-tools;29.0.2" "lldb;3.1" "cmake;3.10.2.4988404" "ndk-bundle" "ndk;20.1.5948944"
-cd -
+[ -z $ROOT_DIR ] && ROOT_DIR=$HOME
+echo "Using ROOT_DIR=\"$ROOT_DIR\""
+
+[ -z $DALI_DIR ] && DALI_DIR=$(cd ..;pwd)
+echo "Using DALI_DIR=\"$DALI_DIR\""
+export DALI_DIR=$DALI_DIR
+
+if [ ! -z $http_proxy ]
+then
+  proxyFull=${http_proxy/http:\/\/}
+  proxyHost=$(echo $proxyFull | cut -d: -f 1)
+  proxyPort=$(echo $proxyFull | cut -d: -f 2)
+  SdbProxyOptions="--proxy=http --proxy_host=$proxyHost --proxy_port=$proxyPort"
+  echo "Proxy detected Host:$proxyHost Port:$proxyPort"
+
+  GRADLE_SETTINGS_DIR=$HOME/.gradle
+  GRADLE_PROPERTIES_FILE=$GRADLE_SETTINGS_DIR/gradle.properties
+  if [ ! -f $GRADLE_PROPERTIES_FILE ]
+  then
+    mkdir -p $GRADLE_SETTINGS_DIR
+    echo "systemProp.http.proxyHost=$proxyHost" >> $GRADLE_PROPERTIES_FILE
+    echo "systemProp.http.proxyPort=$proxyPort" >> $GRADLE_PROPERTIES_FILE
+
+    if [ ! -z $https_proxy ]
+    then
+      httpsProxyFull=${https_proxy/https:\/\/}
+      httpsProxyHost=$(echo $httpsProxyFull | cut -d: -f 1)
+      httpsProxyPort=$(echo $httpsProxyFull | cut -d: -f 2)
+      echo "systemProp.https.proxyHost=$httpsProxyHost" >> $GRADLE_PROPERTIES_FILE
+      echo "systemProp.https.proxyPort=$httpsProxyPort" >> $GRADLE_PROPERTIES_FILE
+    fi
+  fi
 fi
+
+if [ ! -d "$ANDROID_SDK" ]; then
+  if [ ! -d "$ROOT_DIR/Android/Sdk" ]; then
+    mkdir -p "$ROOT_DIR/Android/Sdk"
+    cd "$ROOT_DIR/Android/Sdk"
+    wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+    unzip sdk-tools-linux-4333796.zip
+    tools/bin/sdkmanager --update $SdbProxyOptions
+    yes | tools/bin/sdkmanager $SdbProxyOptions "patcher;v4" "platform-tools" "platforms;android-29" "build-tools;29.0.2" "lldb;3.1" "cmake;3.10.2.4988404" "ndk-bundle" "ndk;20.1.5948944"
+    cd -
+  fi
 fi
 
 if [ ! -d "$ANDROID_SDK" ]; then
 # try default path
-if [ -d "$HOME/Android/Sdk" ]; then
-export ANDROID_SDK=$HOME/Android/Sdk
-fi
+  if [ -d "$ROOT_DIR/Android/Sdk" ]; then
+    export ANDROID_SDK=$ROOT_DIR/Android/Sdk
+  fi
 fi
 
 if [ ! -d "$ANDROID_NDK" ]; then
-if [ -d "$ANDROID_SDK" ]; then
-NDK_DIR=$(find $ANDROID_SDK -maxdepth 2 -name ndk-build | sed 's/\/ndk-build//')
-if [ -d "$NDK_DIR" ]; then
-export ANDROID_NDK=$NDK_DIR
-fi
-fi
+  if [ -d "$ANDROID_SDK" ]; then
+    NDK_DIR=$(find $ANDROID_SDK -maxdepth 2 -name ndk-build | sed 's/\/ndk-build//')
+    if [ -d "$NDK_DIR" ]; then
+      export ANDROID_NDK=$NDK_DIR
+    fi
+  fi
 fi
 
-if [ ! -d "$HOME/gradle/gradle-5.4.1" ]; then
-mkdir -p $HOME/gradle
-cd $HOME/gradle
-wget https://services.gradle.org/distributions/gradle-5.4.1-bin.zip
-unzip gradle-5.4.1-bin.zip
+if [ ! -d "$ROOT_DIR/gradle/gradle-5.4.1" ]; then
+  mkdir -p $ROOT_DIR/gradle
+  cd $ROOT_DIR/gradle
+  wget https://services.gradle.org/distributions/gradle-5.4.1-bin.zip
+  unzip gradle-5.4.1-bin.zip
 cd -
 fi
 
-export PATH=$PATH:$HOME/gradle/gradle-5.4.1/bin
-echo 'sdk.dir='$(echo $ANDROID_SDK) > local.properties
+export PATH=$PATH:$ROOT_DIR/gradle/gradle-5.4.1/bin
+[ ! -f local.properties ] && echo 'sdk.dir='$(echo $ANDROID_SDK) > local.properties
 
-if [ ! -d "$DALI_DIR" ]; then
-export DALI_DIR=$(cd ..;pwd)
-fi
+repos="
+  android-dependencies
+  dali-core
+  dali-adaptor
+  dali-toolkit
+"
 
-if [ ! -d "$DALI_DIR/android-dependencies" ]; then
-git clone https://github.com/dalihub/android-dependencies.git
-fi
+GitHubLocation=https://github.com/dalihub
 
-if [ ! -d "$DALI_DIR/dali-core" ]; then
-git clone --branch devel/master ssh://review.tizen.org:29418/platform/core/uifw/dali-core
-fi
-
-if [ ! -d "$DALI_DIR/dali-adaptor" ]; then
-git clone --branch devel/master ssh://review.tizen.org:29418/platform/core/uifw/dali-adaptor
-fi
-
-if [ ! -d "$DALI_DIR/dali-toolkit" ]; then
-git clone --branch devel/master ssh://review.tizen.org:29418/platform/core/uifw/dali-toolkit
-fi
+for repo in $repos
+do
+  [ ! -d "$DALI_DIR/$repo" ] && git clone $GitHubLocation/$repo.git $DALI_DIR/$repo
+done
 
 gradle wrapper
 if [ -z "$DEBUG" ]; then
