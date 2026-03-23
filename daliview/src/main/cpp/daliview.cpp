c55f7b4c3e63b35f99ff712dfa01f8c7ef0f92ca
Eunki Hong <eunkiki.hong@samsung.com>
2026-03-23 18:53:16 +0900

    Fix compile error due to redefine of keyEvent
    Signed-off-by: Eunki Hong <eunkiki.hong@samsung.com>

--- daliview/src/main/cpp/daliview.cpp
+++ daliview/src/main/cpp/daliview.cpp
@@ -255,18 +255,18 @@ extern "C" JNIEXPORT void JNICALL Java_com_sec_daliview_DaliView_nativeOnKeyEven
       break;
   }
 
+  Dali::KeyEvent keyEvent;
   switch( keyCode )
   {
     case 4:
-      Dali::KeyEvent keyEvent = Dali::DevelKeyEvent::New("XF86Back", "", "", keyCode, 0, timestamp, state, "", "", Dali::Device::Class::NONE, Dali::Device::Subclass::NONE);
-      Dali::Adaptor::Get().FeedKeyEvent( keyEvent );
+      keyEvent = Dali::DevelKeyEvent::New("XF86Back", "", "", keyCode, 0, timestamp, state, "", "", Dali::Device::Class::NONE, Dali::Device::Subclass::NONE);
       break;
     default:
-      Dali::KeyEvent keyEvent = Dali::DevelKeyEvent::New("", "", "", keyCode, 0, timestamp, state, "", "", Dali::Device::Class::NONE, Dali::Device::Subclass::NONE);
-      Dali::Adaptor::Get().FeedKeyEvent( keyEvent );
+      keyEvent = Dali::DevelKeyEvent::New("", "", "", keyCode, 0, timestamp, state, "", "", Dali::Device::Class::NONE, Dali::Device::Subclass::NONE);
       break;
   }
 
+  Dali::Adaptor::Get().FeedKeyEvent( keyEvent );
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_sec_daliview_DaliView_nativeOnFinalize(JNIEnv* jenv, jobject obj, jlong handle)
